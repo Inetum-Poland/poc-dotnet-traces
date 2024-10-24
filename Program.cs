60be4616d6e0bedc55e211be04f93ca509cf2485
Budnikowski Slawomir <slawomir.budnikowski@inetum.com>
2024-10-24 12:05:28 +0200

    added opentelemetry instrumentation

--- Program.cs
+++ Program.cs
@@ -1,7 +1,40 @@
 using BookStoreApi.Models;
 using BookStoreApi.Services;
+using Examples.AspNetCore;
+using OpenTelemetry.Logs;
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+using System.Reflection.PortableExecutable;
 
 var builder = WebApplication.CreateBuilder(args);
+
+const string serviceName = "bookstore-api";
+
+var otlpHostName = Environment.GetEnvironmentVariable("OTLP_HOSTNAME") ?? "localhost";
+
+builder.Logging.ClearProviders()
+    .AddOpenTelemetry(options =>
+    {
+        options.IncludeFormattedMessage = true;
+        options.IncludeScopes = true;
+        options.SetResourceBuilder(ResourceBuilder.CreateDefault().AddService(serviceName))
+        .AddOtlpExporter(options => options.Endpoint = new Uri($"http://{otlpHostName}:4317"))
+        .AddConsoleExporter();
+    });
+
+builder.Services.AddOpenTelemetry()
+      .ConfigureResource(resource => resource.AddService(serviceName))
+      .WithTracing(tracing => tracing
+          .AddAspNetCoreInstrumentation()
+          .AddOtlpExporter(options => options.Endpoint = new Uri($"http://{otlpHostName}:4317"))
+          .AddConsoleExporter())
+      .WithMetrics(metrics => metrics
+          .AddAspNetCoreInstrumentation()
+          .AddOtlpExporter(options => options.Endpoint = new Uri($"http://{otlpHostName}:4317"))
+          .AddConsoleExporter());
+
+builder.Services.AddSingleton<Instrumentation>();
 
 builder.Services.Configure<BookStoreDatabaseSettings>(
   builder.Configuration.GetSection("BookStoreDatabase"));
